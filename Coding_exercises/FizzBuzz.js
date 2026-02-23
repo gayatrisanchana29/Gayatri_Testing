0a29c4d038b861d154f5ee9c474e39de1d3b996b
gayatrisanchana29 <gayatrisanchana29@gmail.com>
2026-02-23 21:58:49 +0530

    Add coding exercises: Grade Calculator, Program1Grade and FizzBuzz

---
+++ Coding_exercises/FizzBuzz.js
@@ -0,0 +1,12 @@
+for (let Num = 1; Num <= 100; Num++) {
+    if (Num % 3 == 0) {
+        console.log("Fizz");
+
+    } else if (Num % 5 == 0) {
+        console.log("Buzz");
+
+    }
+    else {
+        console.log(Num);
+    }
+}
---
+++ Coding_exercises/Grade_Calculator.js
@@ -0,0 +1,20 @@
+let score = 66;
+if (score >= 90 && score <= 100) {
+
+    console.log("Grade A");
+}
+else if (score >= 80 && score <= 89) {
+    console.log("Grade B");
+}
+else if (score >= 70 && score <= 70) {
+    console.log("Grade C");
+}
+else if (score >= 60 && score <= 69) {
+    console.log("Grade D");
+}
+else if (score >= 0 && score <= 59) {
+    console.log("Grade F");
+}
+else {
+    console.log("Invalid Score");
+}
