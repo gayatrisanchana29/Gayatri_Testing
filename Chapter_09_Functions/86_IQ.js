6a671073966c321059f5106aea69946d309ca67e
gayatrisanchana29 <gayatrisanchana29@gmail.com>
2026-03-26 07:24:23 +0530

    Add Chapter 09 Functions

---
+++ Chapter_09_Functions/86_IQ.js
@@ -0,0 +1,9 @@
+
+function runTest(name, status, duration) {
+    return `${name}: ${status} (${duration}ms)`;
+}
+
+// Arguments
+console.log(runTest("Login", "pass", 320));
+
+// "Login: pass (320ms)"
